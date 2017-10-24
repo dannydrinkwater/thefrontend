421d2048d365f3d3742e24ea56e44861e4c3efe7
Danny Drinkwater <danny.drinkwater@gmail.com>
2017-10-24 22:12:30 +0100

    Added Gulp JS task

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp      = require('gulp');
+var sass      = require('gulp-sass');
 var minifyCSS = require('gulp-cssnano');
-
-gulp.task('html', function(){
-  return gulp.src('source/example.html')
-    .pipe(gulp.dest('public/'))
-});
+var concat    = require('gulp-concat');
+var uglify    = require('gulp-uglify');
 
 gulp.task('css', function(){
   return gulp.src('source/scss/main.scss')
     .pipe(sass())
     .pipe(minifyCSS())
-    .pipe(gulp.dest('public/css'))
+    .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('default', [ 'html', 'css' ]);
+gulp.task('js', function() {
+  return gulp.src(['source/js/plugins/*.js', 'source/js/main.js'])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('default', [ 'css', 'js' ]);
