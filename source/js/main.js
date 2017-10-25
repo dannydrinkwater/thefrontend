6dadb60eebdb459afc19fc602a7b6dba02b0e109
Danny Drinkwater <danny.drinkwater@gmail.com>
2017-10-25 21:11:03 +0100

    Add Monitor Width component - allows JS to quickly determine which breakpoint is currently being applied.

--- source/js/main.js
+++ source/js/main.js
@@ -3,18 +3,48 @@ var site = {
 		$body: $('body'),
     	$window: $(window)
 	},
-	a {
-		init: function () {
-			var self = this;
 
-			site.b();
-		}
+	mq: {
+	    $body: $('body'),
+	    $window: $(window),
+	    $detector: $('#monitor-width'),
+	    detectorWidth: 0,
+
+	    // Current break point of the page
+	    currentBreakpoint: 0,
+	    previousBreakpoint: 0,
+	    
+	    // Breakpoint variables (should match Sass MQ)
+	    mobile: 320,
+	    phablet: 576,
+	    tablet: 768,
+	    desktop: 992,
+	    wide: 1200,
+
+	    init: function() {
+	        var self = this;
+	        mq.monitorWidth();
+	        self.$window.on('resize.mq', function() {
+	            mq.monitorWidth();
+	        });
+	    },
+	    monitorWidth: function() {
+	        var self = this;
+	        if (!self.$detector.length) {
+	            self.$body.append('<div id="monitor-width"></div>');
+	            self.$detector = $('#monitor-width');
+	        }
+	        self.detectorWidth = self.$detector.width();
+	        if (self.detectorWidth !== self.currentBreakpoint) {
+	            // A change has occurred so update the comparison variable
+	            self.previousBreakpoint = self.currentBreakpoint;
+	            self.currentBreakpoint = self.detectorWidth;
+	        }
+	    }
 	},
-	b: function () {
-		var self = this;
-	},
+
 	ready: function () {
-		site.a.init();
+		site.mq.init();
 	}
 }
 
