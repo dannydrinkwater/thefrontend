f5c0a3f7fba09001ccea991c341b6260eef9c165
Dan Drinkwater <daniel.drinkwater@eurocamp.com>
2017-10-30 16:39:42 +0000

    Implement image minification

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass      = require('gulp-sass');
 var minifyCSS = require('gulp-cssnano');
 var concat    = require('gulp-concat');
 var uglify    = require('gulp-uglify');
+var imagemin  = require('gulp-imagemin');
 
 gulp.task('css', function(){
   return gulp.src('source/scss/main.scss')
@@ -22,4 +23,10 @@ gulp.task('js', function() {
     .pipe(gulp.dest('public/js'));
 });
 
-gulp.task('default', [ 'css', 'js' ]);
+gulp.task('img', () =>
+    gulp.src('source/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/img'))
+);
+
+gulp.task('default', [ 'css', 'js', 'img' ]);
